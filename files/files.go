9036690a048f1635df56837e213e5b41da19f299
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2026-04-16 11:28:57 +0200

    feat(files): add Unzip function

--- files/files.go
+++ files/files.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"archive/zip"
 	"fmt"
 	"os"
 	"os/exec"
@@ -47,5 +48,70 @@ func MarkdownToPDF(markdownFilePath string) error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+// Unzip unzip the src into dest
+func Unzip(src, dest string) error {
+	dest = filepath.Clean(dest) + string(os.PathSeparator)
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(dest, 0755)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		path := filepath.Join(dest, f.Name)
+		// Check for ZipSlip: https://snyk.io/research/zip-slip-vulnerability
+		if !strings.HasPrefix(path, dest) {
+			return fmt.Errorf("%s: illegal file path", path)
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
